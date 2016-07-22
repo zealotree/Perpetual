27295d0ad47c258121fc53e69a14593452ee324e
Zealotree <zealotree@gmail.com>
2016-07-22 14:36:31 -0400

    Added basalt

--- src/Perpetual.c
+++ src/Perpetual.c
@@ -1,13 +1,28 @@
 #include <pebble.h>
 Window *my_window;
-static Layer *daily_ring;
-static Layer *hollow_dots;
+static Layer *date_dots;
 static Layer *clock_layer;
 
 typedef struct {
   int hours;
   int minutes;
 } Time;    
+
+// Layout boundaries
+#define HOUR_RING PBL_IF_ROUND_ELSE(50, 50) // HOUR HAND
+#define MIN_RING PBL_IF_ROUND_ELSE(32, 32) // MIN HAND
+#define MONTH_RING PBL_IF_ROUND_ELSE(14, 5) 
+#define MONTH_DOT_RADIUS PBL_IF_ROUND_ELSE(4, 4)
+#define RING_31 PBL_IF_ROUND_ELSE(27, 16) // 31st DOT
+#define MON_RING PBL_IF_ROUND_ELSE(68, 68) 
+#define MON_DOT_INSET PBL_IF_ROUND_ELSE(34, 30) 
+#define DOW_RING PBL_IF_ROUND_ELSE(27, 27) 
+#define AP_RING PBL_IF_ROUND_ELSE(52, 50) 
+#define MARKER_RING PBL_IF_ROUND_ELSE(37, 37) 
+#define MARKER_RING_INSET PBL_IF_ROUND_ELSE(21, 16) 
+#define CONNECTOR_RADIUS PBL_IF_ROUND_ELSE(9, 8) 
+#define DIAL_STROKE PBL_IF_ROUND_ELSE(2, 3) 
+#define DIAL_RADIUS PBL_IF_ROUND_ELSE(24, 22) 
 
 static Time s_last_time;
 
@@ -21,35 +36,30 @@ static int32_t get_angle_for_minute(int min) {
   return (min * 360) / 60;
 }
 
-
-static int32_t get_angle_for_month(int month) {
-  // Progress through 12 months, out of 360 degrees
-  return (month * 360) / 12;
-}
-
 static int32_t get_angle_for_day(int day) {
   return (day * 360) / 30;
 }
 
 
 static void draw_clock(Layer *layer, GContext *ctx) {
-  time_t temp = time(NULL);
+  //time_t temp = time(NULL);
+  time_t temp = 1461738634;
   struct tm *tick_time = localtime(&temp);
 
+  // Set Layout Boundaries
   GRect bounds = layer_get_bounds(layer);
-  GRect hour_ring = grect_inset(bounds, GEdgeInsets(50));  
-  GRect min_ring = grect_inset(bounds, GEdgeInsets(32));  
-  GRect mon_ring = grect_inset(bounds, GEdgeInsets(68));
-  GRect dow_ring = grect_inset(bounds, GEdgeInsets(27));
-
-
-  GRect ap_ring = grect_inset(bounds, GEdgeInsets(52));
-  GRect marker_ring = grect_inset(bounds, GEdgeInsets(38));
-  GRect marker_inset_ring = grect_inset(marker_ring, GEdgeInsets(21));  
+  GRect hour_ring = grect_inset(bounds, GEdgeInsets(HOUR_RING));
+  GRect min_ring = grect_inset(bounds, GEdgeInsets(MIN_RING));  
+  GRect mon_ring = grect_inset(bounds, GEdgeInsets(MON_RING));
+  GRect dow_ring = grect_inset(bounds, GEdgeInsets(DOW_RING));
+  GRect ap_ring = grect_inset(bounds, GEdgeInsets(AP_RING));
+  GRect marker_ring = grect_inset(bounds, GEdgeInsets(MARKER_RING));
+  GRect marker_inset_ring = grect_inset(marker_ring, GEdgeInsets(MARKER_RING_INSET));  
   GRect inset_ring = grect_inset(marker_ring, GEdgeInsets(26));
+  GRect mon_ring_r = grect_inset(marker_ring, GEdgeInsets(MON_DOT_INSET));
+  GPoint center = grect_center_point(&marker_inset_ring);
 
-  GPoint center = grect_center_point(&bounds);
-
+  // Draw the AM/PM indicator
   if (tick_time->tm_hour > 12) {
     graphics_context_set_fill_color(ctx, GColorBlack);
   } else {
@@ -103,13 +113,13 @@ static void draw_clock(Layer *layer, GContext *ctx) {
   }
   
   // Draw the Center Dial
-  graphics_context_set_stroke_width(ctx, 3);
+  graphics_context_set_stroke_width(ctx, DIAL_STROKE);
   graphics_context_set_fill_color(ctx, GColorBrass);
-  graphics_draw_circle(ctx, center, 22);
+  graphics_draw_circle(ctx, center, DIAL_RADIUS);
   
   // Draw Center Circle
   graphics_context_set_fill_color(ctx, GColorWhite);
-  graphics_fill_circle(ctx, center, 9);
+  graphics_fill_circle(ctx, center, CONNECTOR_RADIUS);
 
 
   
@@ -117,11 +127,9 @@ static void draw_clock(Layer *layer, GContext *ctx) {
   s_last_time.hours -= (s_last_time.hours > 12) ? 12 : 0;
   s_last_time.minutes = tick_time->tm_min;
   
-  // Draw Month Outline
   
   // Fill Current Month
-  GRect mon_ring_r = grect_inset(mon_ring, GEdgeInsets(6));
-  int month_angle = DEG_TO_TRIGANGLE(get_angle_for_month(tick_time->tm_mon + 1));
+  int month_angle = DEG_TO_TRIGANGLE(get_angle_for_hour(tick_time->tm_mon + 1));
   const GPoint h3 = gpoint_from_polar(
       mon_ring_r,
       GOvalScaleModeFillCircle,
@@ -163,13 +171,13 @@ static void draw_clock(Layer *layer, GContext *ctx) {
 }
 
 
-static void draw_hollow_dots(Layer *layer, GContext *ctx) {
+static void draw_date_dots(Layer *layer, GContext *ctx) {
   time_t temp = time(NULL);
   struct tm *tick_time = localtime(&temp);
 
   GRect bounds = layer_get_bounds(layer);
-  GRect month_ring = grect_inset(bounds, GEdgeInsets(14));
-  GRect ring31 = grect_inset(bounds, GEdgeInsets(27));  
+  GRect month_ring = grect_inset(bounds, GEdgeInsets(MONTH_RING));
+  GRect ring31 = grect_inset(bounds, GEdgeInsets(RING_31));  
   
   for(int i = 0; i < 32; i++) {
     int day_angle = DEG_TO_TRIGANGLE(get_angle_for_day(i));
@@ -188,7 +196,7 @@ static void draw_hollow_dots(Layer *layer, GContext *ctx) {
       } else {
         graphics_context_set_fill_color(ctx, GColorBlack);
       }
-      graphics_fill_circle(ctx, fill_in, 4);
+      graphics_fill_circle(ctx, fill_in, MONTH_DOT_RADIUS);
 
     } else {
       GRect square = grect_centered_from_polar(month_ring, GOvalScaleModeFitCircle, day_angle, GSize(12,12));
@@ -200,7 +208,7 @@ static void draw_hollow_dots(Layer *layer, GContext *ctx) {
       } else {
         graphics_context_set_fill_color(ctx, GColorBlack);
       }
-      graphics_fill_circle(ctx, fill_in, 4);
+      graphics_fill_circle(ctx, fill_in, MONTH_DOT_RADIUS);
     }
     
   }
@@ -212,38 +220,31 @@ static void main_window_load() {
   window_set_background_color(my_window, GColorDarkGray);
   Layer *window_layer = window_get_root_layer(my_window);
   GRect bounds = layer_get_bounds(window_layer);
-  daily_ring = layer_create(bounds);
-  hollow_dots = layer_create(bounds);
+  date_dots = layer_create(bounds);
   clock_layer = layer_create(bounds);
   
-  //layer_set_update_proc(daily_ring, draw_current_date);
-  layer_set_update_proc(hollow_dots, draw_hollow_dots);
+  layer_set_update_proc(date_dots, draw_date_dots);
   layer_set_update_proc(clock_layer, draw_clock);
 
-
-  layer_add_child(window_get_root_layer(my_window), hollow_dots);
-  layer_add_child(window_get_root_layer(my_window), daily_ring);
+  layer_add_child(window_get_root_layer(my_window), date_dots);
   layer_add_child(window_get_root_layer(my_window), clock_layer);
-  layer_mark_dirty(daily_ring);
-  layer_mark_dirty(hollow_dots);
+  layer_mark_dirty(date_dots);
   layer_mark_dirty(clock_layer);
 }
 
 static void main_window_unload() {
-  layer_destroy(daily_ring);
-  layer_destroy(hollow_dots);
+  layer_destroy(date_dots);
   layer_destroy(clock_layer);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits changed) {
-  layer_mark_dirty(daily_ring);
-  layer_mark_dirty(hollow_dots);
+  layer_mark_dirty(date_dots);
   layer_mark_dirty(clock_layer);
 }
 
 static void app_connection_handler(bool connected) {
   if (! connected) {
-      static const uint32_t const segments[] = { 1000, 500, 400, 300, 200 };
+      static const uint32_t const segments[] = { 1000, 900, 250 };
       VibePattern pat = {
         .durations = segments,
         .num_segments = ARRAY_LENGTH(segments),
@@ -263,7 +264,6 @@ void handle_init(void) {
   });
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
   window_stack_push(my_window, true);
-  layer_mark_dirty(daily_ring);
 }
 
 void handle_deinit(void) {
